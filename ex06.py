914b7e9876d823cc0a7d87c4409befe7f674cda4
GUARACI DALL OSTO <guara2100@gmail.com>
2020-04-26 14:12:56 -0300

    ex05 e 06 Strings e textos

---
+++ ex06.py
@@ -0,0 +1,16 @@
+types_of_people = 10
+x = f"there are {types_of_people} types of people."
+binary = "binary"
+do_not = "dont't"
+y = f"those who know {binary} and theose who {do_not}."
+print(x)
+print(y)
+print(f"said: {x}")
+print(f"I also said: '{y}'")
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w + e)
