e51879d893b7b3876b15f268f9b6d2d54ff41a77
GUARACI DALL OSTO <guara2100@gmail.com>
2020-04-27 21:31:03 -0300

    Imprimindo,Imprimindo,Imprimindo

---
+++ ex09.py
@@ -0,0 +1,11 @@
+# Aqui temos uma coisa nova estranha, lembre-se de digitá-la exatamente.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJun\nAug"
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("""
+There's someting going on here.
+With the tree doble-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
